7e096f48df11996575071070f8f9bcf600de4f71
Nabil <duomaster01@gmail.com>
2022-07-18 00:00:33 -0700

    cleaned up the code a bit more:

--- routes/user/user.js
+++ routes/user/user.js
@@ -5,12 +5,12 @@ const userRouter = express.Router();
 // url: http://localhost:3000/user
 userRouter.put('/', (req, res) => {
     res.send('Got a PUT request at /user');
-  })
+});
 
 // url: http://localhost:3000/user/inventory
 userRouter.delete('/inventory', (req, res) => {
   res.send('Got a DELETE request at /user/inventory');
-})
+});
 
 module.exports = userRouter;
   
